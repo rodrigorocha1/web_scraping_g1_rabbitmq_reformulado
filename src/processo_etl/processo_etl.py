d4b7733d3bc98948732d276ec58e9aa591bae3b5
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-11-01 19:01:13 -0300

    Identação de dados

--- src/processo_etl/processo_etl.py
+++ src/processo_etl/processo_etl.py
@@ -55,29 +55,30 @@ class ProcessoEtl:
                 raise ValueError(f"Fila desconhecida: {nome_fila}")
 
     def processar_noticia(self, url: str, chave_links_processados: str, nome_fila: str) -> Tuple[bool, str]:
-        try:
+
             self.__servico_web_scraping.url = url
-            dados = self.__servico_web_scraping.abrir_conexao()
-            if dados:
-                noticia = self.__servico_web_scraping.obter_dados(dados=dados)
-                if noticia.texto is not None and len(noticia.texto) > 0:
-                    if noticia.data_hora is not None:
-                        score = int(noticia.data_hora.timestamp())
-                    else:
-                        score = 0
-                    params = {
-                        'score': score,
-                        'valor': url
-                    }
-                    id_site = self.__escolha_id_site(nome_fila=nome_fila)
-                    consulta = self.__scripts_banco.realizar_insercao_lote(id_site=id_site, param=noticia)
+            try:
+                dados = self.__servico_web_scraping.abrir_conexao()
+                if dados:
+                    noticia = self.__servico_web_scraping.obter_dados(dados=dados)
+                    if noticia.texto is not None and len(noticia.texto) > 0:
+                        if noticia.data_hora is not None:
+                            score = int(noticia.data_hora.timestamp())
+                        else:
+                            score = 0
+                        params = {
+                            'score': score,
+                            'valor': url
+                        }
+                        id_site = self.__escolha_id_site(nome_fila=nome_fila)
+                        consulta = self.__scripts_banco.realizar_insercao_lote(id_site=id_site, param=noticia)
 
-                    self.__conexao_banco.gravar_registro(chave=id_site, dados=consulta)
-                    self.__conexao_log.enviar_url_processada(
-                        chave=chave_links_processados,
-                        params=params
-                    )
+                        self.__conexao_banco.gravar_registro(chave=id_site, dados=consulta)
+                        self.__conexao_log.enviar_url_processada(
+                            chave=chave_links_processados,
+                            params=params
+                        )
 
-            return True, 'Sucesso'
-        except Exception as e:
-            return False, f'Erro: {str(e)}'
+                return True, 'Sucesso'
+            except Exception as e:
+                return False, f'Erro: {str(e)}'
